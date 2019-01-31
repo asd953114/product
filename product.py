421e8a817ee8ac96f6c1eb2c030788381493adce
asd953114 <asd953114@gmail.com>
2019-01-31 18:50:00 +0800

    add header

--- product.py
+++ product.py
@@ -6,6 +6,7 @@ while True:
     if name == "q":
     	break
     price = input("請輸入商品價格: ")	
+    prince = int(price)
     products.append([name, price])
 
 
@@ -14,4 +15,9 @@ print(products)
 
 for p in products:
 	print(p[0], "的價格是", p[1])
-
+#with 就是自動幫你close 因為在程式語言裡通常你打開檔案你就要關閉
+#但大家很常忘記，所以python用了with幫你直接關閉
+with open("products.csv", "w", encoding="utf-8") as f:  #w寫入模式 r讀取模式
+	f.write("商品,價格\n")
+	for p in products:
+		f.write(p[0] + "," + str(p[1]) + "\n")
